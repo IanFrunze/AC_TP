5b0d2640ce20d6fdaddbc4de331fdc19785c5f28
A53118-ISEL <A53118@alunos.isel.pt>
2025-03-25 23:01:18 +0000

    Pre-final

--- T_Prático_1/tp_1.s
+++ T_Prático_1/tp_1.s
@@ -10,6 +10,23 @@ program:
 stack_top_addr:
     .word stack_top
 
+/*
+uint32_t umull32( uint32_t M, uint32_t m ) {
+    int64_t M_ext = M;
+    int64_t p = m;
+    uint8_t p_1 = 0;
+    for( uint16_t i = 0; i < 32; i++ ) {
+        if ( ( p & 0x1 ) == 0 && p_1 == 1 ) {
+            p += M_ext << 32;
+        } else if ( ( p & 0x1 ) == 1 && p_1 == 0 ) {
+            p -= M_ext << 32;
+        }
+        p_1 = p & 0x1;
+        p >>= 1;
+    }
+    return p;
+}
+*/
 
 umull32:
     ; 32 bits menor peso do M_ext:
@@ -83,24 +100,25 @@ uint16_t rand( void ) {
     return ( seed >> 16 );
 }
 */
+
 rand:
-    ldr     r0,seed0_addr
-    ldr     r0,[r0]
-    ldr     r1,seed1_addr
-    ldr     r1,[r1]
+    ldr     r0, seed0_addr
+    ldr     r0, [r0]
+    ldr     r1, seed1_addr
+    ldr     r1, [r1]
     ; umull32(seed,214013) está em r0 e r
-    mov     r2,#0xFD
-    movt    r2,#0x43
-    mov     r3,#0x03
+    mov     r2, #0xFD
+    movt    r2, #0x43
+    mov     r3, #0x03
     push    lr
-    bl       umull32
+    bl      umull32
     pop     lr
     ; (umull32(seed,214013) + 2531011)
-    mov     r2,#0xC3
-    movt    r2,#0x9E
-    mov     r3,#0x26
-    add     r0,r0,r2
-    adc     r1,r1,r3
+    mov     r2, #0xC3
+    movt    r2, #0x9E
+    mov     r3, #0x26
+    add     r0, r0, r2
+    adc     r1, r1, r3
     ; ..% RAND_MAX
     for_init:
     mov     r2, #0xFF
@@ -108,26 +126,27 @@ rand:
     mov     r3, #0
     b       for_cond
     for:
-    sub     r0,r0,r2
-    sbc     r1,r1,r3
+    sub     r0, r0, r2
+    sbc     r1, r1, r3
     for_cond:
-    cmp     r0,r2
-    sbc     r4,r1,r3
+    cmp     r0, r2
+    sbc     r4, r1, r3
     bhs     for
     ; seed = ...
-    mov     r4,sp
-    ldr     r5,seed0_addr
-    ldr     sp,[r5]
+    mov     r4, sp
+    ldr     r5, seed0_addr
+    ldr     sp, [r5]
     pop     r5
     pop     r5
-    mov     r7,r0
+    mov     r7, r0
     push    r1
     push    r0
-    mov     sp,r4
+    mov     sp, r4
     ;seed >> 16
-    mov     r0,r7
-    mov     pc,lr
+    mov     r0, r7
+    mov     pc, lr
     b .
+
 /*
 int main( void ) {
     uint8_t error = 0;
