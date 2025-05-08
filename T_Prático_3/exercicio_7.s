c40a67f9609393483e78484e0b488b9896e91451
IanFrunze <ian.frunze06@gmail.com>
2025-05-08 18:11:54 +0100

    Otimização do exercício 7

--- T_Prático_3/exercicio_7.s
+++ T_Prático_3/exercicio_7.s
@@ -1,41 +1,40 @@
     .equ    OUTPORT_ADDR, 0xF800 ; addr do config do stor, alterar depois 
     .equ    INPORT_ADDR, 0xFC00 ; addr do config do stor, alterar depois
-    
+    .equ    FRIST_3_BITS, 0x7
+
     .text
-    b   programa
+    b       main
     b .
-programa:
-    b   main
+
+main:
+main_loop:
+    bl      inport_read
+    mov     r4, #FRIST_3_BITS
+    and     r0, r0, r4
+    bl      construct_out
+    bl      outport_write
+    b       main_loop
 
 inport_read:
     ldr     r0, inport_addr
     ldr     r0, [r0]
     mov     pc, lr
-inport_addr:
-    .word   INPORT_ADDR
-
-;recebe um byte em r0 e retorna em r0 apenas os 3 primeiros bits
-get_bits:
-    mov     r1, #0x7
-    and     r0, r0, r1
-    mov     pc, lr
 
 ;recebe uma valor no registo r0, e controi um byte 
 ;em que o bit com indice r0 fica a 0 e tudos os outros bits ficam a 1
 construct_out:
-    ;r1 = 0xFE -> 1111 1110
-    mov     r1, #0xFE
+    ;r1 = 1 -> 0000 0001
+    mov     r1, #1
 loop_init:
-    and     r0,r0,r0
-    bzs     loop_end
+    b       loop_cond
 loop:
     lsl     r1, r1, #1
-    add     r1, r1, #1
     sub     r0, r0, #1
+loop_cond:
     and     r0, r0, r0
     bzc     loop
-loop_end:
-    mov     r0, r1
+    mov     r0, #0xFF
+    eor     r0, r1, r0
     mov     pc, lr
 
 ;recebe em r0 um byte, e escreve o que está em r0 no porto de saída
@@ -44,20 +43,7 @@ outport_write:
     strb    r0, [r1,#1]
     mov     pc, lr
 
+inport_addr:
+    .word   INPORT_ADDR
 outport_addr:
     .word   OUTPORT_ADDR
-
-main:
-
-main_loop:
-    bl      inport_read
-    bl      get_bits
-    bl      construct_out
-    bl      outport_write
-    b       main_loop
-    b .
-
-
-
-
-
