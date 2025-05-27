537529abeb7ce98ecb4cfd9493bf65a621483da9
A53118-ISEL <A53118@alunos.isel.pt>
2025-05-27 16:16:04 +0100

    Projeto update_2

--- Projeto/programa.s
+++ Projeto/programa.s
@@ -3,10 +3,9 @@
 ; Autor:     Ian Frunze (A52867@alunos.isel.pt), Tito Silva (A53118@alunos.isel.pt)
 ; Data:      XX-05-2025
 
-
 ; Roll animation available:
-; Roullete - BCDEFA 
-; Spin - (Mostra sequencialmente as faces do dado)
+; Roulette - BCDEFA 
+; Spin - (Mostra sequencialmente as faces do dado)(Opcional)
 
 ; Definição dos valores dos símbolos utilizados no programa
 
@@ -14,7 +13,7 @@
 
 	.equ	ENABLE_INTERRUPT, 0x10 ; --------00 0(M) 1(I) 0(N) 0(V) 0(C) 0(Z) , Mete a Flag I (Interrupt Enable) a 1 e a Flag M (Mode) a 0
 
-    .equ    INPORT_ADDRESS, 0xFF80 ; Endereco do porto de entrada
+	.equ    INPORT_ADDRESS, 0xFF80 ; Endereco do porto de entrada
 	.equ	OUTPORT_ADDRESS, 0xFFC0 ; Endereco do porto de saida
 
 	.equ 	SIDES_MASK, 0x0C ; Máscara para os bits que controlam lados do dado
@@ -26,11 +25,8 @@
 
 	.equ 	SLEEP_DURATION, 0x10 ; 2 segundos de sleep
 
-;	.equ	VAR_INIT_VAL, 0               ; Valor inicial de var
-
 ; Secção:    text
-; Descrição: a
-;
+
 	.text
 	b 		program
 	b 		interrupt_routine_addr
@@ -45,9 +41,7 @@ interrupt_routine_addr:
 	.word 	interrupt_routine
 
 main:
-;	Ativa interrupt_routine (ou seja, vê o roll a 1)
-
-
+;	Ativa interrupt_routine
 	mrs		r0, cpsr
 	mov 	r1, #ENABLE_INTERRUPT
 	orr 	r0, r0, r1
@@ -56,27 +50,35 @@ main_loop:
 	b 		inport_read
 	b		roll_check
 	b 		sides_read
-;	b		random_face
-;	b		outport_write
-;	b		main
+;	se o dado atual == dado anterior itera de novo
+if:
+	cmp 	r4, r0
+	bzs 	if_end
+	mov 	r4, r0
+;	b		random_face (esperar pelo stor)
+if_end:
+ 	b		outport_write
+	b		main
 
-; função folha
-; lê e retorna o porto de entrada em r0
+; Rotina:    import_read
+; Descricao: Lê o porto de entrada a 8 bits o valor passado como argumento. Função folha.
+; Entradas:  -
+; Saidas:    r0 - valor do porto de saida
+; Efeitos:   -
 inport_read:
-	ldr		r0, #INPORT_ADDRESS
+	ldr		r0, [INPORT_ADDRESS, #0]
 	ldrb 	r0, [r0, #0]
 	mov		pc, lr
 
-; função folha
 ; Verifica se o roll é 0, caso seja, usa esse sinal diretamente para ativar o FED, e por sua vez a rotina de interrupção
 roll_check:
 	mov 	r1, #ROLL_MASK
 	and 	r0, r0, r1
 	mov 	r1, #FED_ADDRESS
+	movt 	r1, #FED_ADDRESS
 	str		r0, [r1, #0]
 	mov		pc, lr
 
-; função folha
 ; recebe r0 (porto de entrada) como parâmetro
 sides_read:
 	mov 	r1, #SIDES_MASK
@@ -85,7 +87,7 @@ sides_read:
 	ldr 	r1, die_sides_addr
 	ldrb 	r0, [r1, r0]
 	mov 	pc, lr
-; retorna com o número de lados em r0
+; retorna com o número de lados em r0 e r4
 
 die_sides_addr:
 	.word	die_sides
@@ -93,6 +95,21 @@ die_sides_addr:
 seg7_values_addr:
 	.word	seg7_values
 
+interrupt_routine: ; Faz efeito luminoso (2s, 1s por spin) e mede tempos
+	push 	r0
+	mov		r0, #FED_ADDRESS & 0xFF
+	movt	r0, #(FED_ADDRESS >> 8) & 0xFF
+
+	b		animation
+
+	b 		outport_write
+;	b 		(uma função que observe o valor var proveniente do atb "oscilator" para fazer os 10s com uma frequência alta)
+	pop 	r0
+	movs	pc, lr
+
+animation:
+	; fzr loop para o roulete
+	
 ; Rotina:    outport_write
 ; Descricao: Escreve num porto de saida a 8 bits o valor passado como argumento.
 ;            Interface exemplo: void outport_write( uint8_t value );
@@ -105,15 +122,15 @@ outport_write:
 	strb	r0, [r1, #0]
 	mov		pc, lr
 
-interrupt_routine: ; Faz efeito luminoso (2s, 1s por spin) e mostra a face que calhou
-	push 	r0
-	mov		r0, #FED_ADDRESS & 0xFF
-	movt	r0, #(FED_ADDRESS >> 8) & 0xFF
-
-	pop 	r0
-	movs	pc, lr
-
 	.data
+animation_seq: ; Roullete Style
+	.byte 	0x02 ; B
+	.byte 	0x04 ; C
+	.byte 	0x08 ; D
+	.byte 	0x10 ; E
+	.byte 	0x20 ; F
+	.byte   0x01 ; A
+
 die_sides:
 	.byte 	0x04 ; 4 lados 
 	.byte 	0x06 ; 6 lados
@@ -146,6 +163,8 @@ die_8:
 	.byte 	0x06 ; 6
 	.byte 	0x07 ; 7
 	.byte 	0x08 ; 8
+
+; DADO VICIADO, O DADO DE 12 FACES DEVE SER O DE 6, 2 VEZES
 
 ; Dado 12 faces = {1,2,2,3,4,4,5,6,6,7,8,8} (Ímpares 1 vez e Pares 2 vezes, exceto o 9)
 die_12:
